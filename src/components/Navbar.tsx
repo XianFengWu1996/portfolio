d7af90c3d3b8ec66fdef4c47a6b20142f003c1d6
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-01-23 17:12:46 -0500

    rename asset image folder

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+
+const NavBar = () => {
+  return (
+    <div className="fixed w-full h-20 shadow-xl z-[100]">
+      <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
+        <Image
+          src="/assets/images/navLogo.png"
+          alt="/"
+          width="125"
+          height="50"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
