f9fa04b8eceecdc9f9e17a5fa1017eb4717790a7
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-01-24 01:27:43 -0500

    finish project section

---
+++ src/components/Projects.tsx
@@ -0,0 +1,51 @@
+import propertyImg from 'public/assets/images/projects/property.jpg';
+import cryptoImg from 'public/assets/images/projects/crypto.jpg';
+import netflixImg from 'public/assets/images/projects/netflix.jpg';
+import twitchImg from 'public/assets/images/projects/twitch.jpg';
+
+import ProjectItem from './ProjectItem';
+
+const Projects = () => {
+  return (
+    <div className="w-full">
+      <div className="max-w-[1240px] mx-auto px-2 py-16">
+        <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+          Project
+        </p>
+        <h2 className="py-4">What I&apos;ve Built</h2>
+
+        <div className="grid md:grid-cols-2 gap-8">
+          <ProjectItem
+            title="Property Finder"
+            lang="React JS"
+            imgSrc={propertyImg}
+            link="/"
+          />
+
+          <ProjectItem
+            title="Crypto App"
+            lang="React JS"
+            imgSrc={cryptoImg}
+            link="/"
+          />
+
+          <ProjectItem
+            title="Netflix Clone"
+            lang="React JS"
+            imgSrc={netflixImg}
+            link="/"
+          />
+
+          <ProjectItem
+            title="Twitch Clone"
+            lang="React JS"
+            imgSrc={twitchImg}
+            link="/"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
