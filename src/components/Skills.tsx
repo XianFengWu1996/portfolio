3515d500fbaf38cd91a43524849a447324fe08c0
XianFengWu1996 <46692996+XianFengWu1996@users.noreply.github.com>
2023-01-24 00:44:48 -0500

    finish skills sections

---
+++ src/components/Skills.tsx
@@ -0,0 +1,154 @@
+import Image from 'next/image';
+
+const Skills = () => {
+  return (
+    <div className="w-full lg:h-screen p-2">
+      <div className="max-w-[1240px] mx-auto flex flex-col justify-center h-full">
+        <p className="uppercase text-xl tracking-widest text-[#5651e5]">
+          Skills
+        </p>
+        <h2 className="py-4">What I Can Do</h2>
+
+        <div className="grid md:grid-col-2 lg:grid-cols-4 gap-8">
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/html.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>HTML</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/css.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>CSS</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/javascript.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>Javascript</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/node.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>Node Js</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/firebase.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>Firebase</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/react.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>React</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/mongo.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>Mongo</h3>
+              </div>
+            </div>
+          </div>
+
+          <div className="p-6 shadow-xl rounded-xl hover:scale-105 ease-in duration-300">
+            <div className="grid grid-cols-2 gap-4 justify-center items-center">
+              <div className="m-auto">
+                <Image
+                  src={'/assets/images/skills/github1.png'}
+                  height={64}
+                  width={64}
+                  alt="Skill Image"
+                />
+              </div>
+
+              <div className="flex flex-col items-center justify-center">
+                <h3>Github</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import { Inter } from '@next/font/google';
 import NavBar from '@/components/Navbar';
 import Main from '@/components/Main';
 import About from '@/components/About';
+import Skills from '@/components/Skills';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -19,6 +20,7 @@ export default function Home() {
       <NavBar />
       <Main />
       <About />
+      <Skills />
     </>
   );
 }
